6204f23f3d419561d3daf562bcf45df51a7d4a49
ConnorJFlynn <Connor.Flynn@pnnl.gov>
2015-07-15 22:33:54 -0700

    igor2serial
    This works as confirmed by direct examination of SP2 files having
    calendar date/time and igor time.

--- anc_tools/utils/igor2serial.m
+++ anc_tools/utils/igor2serial.m
@@ -1,38 +1,7 @@
  
 function [serial] = igor2serial (igor_time)
 
-% Converts epoch time to serial time.
-%
-%     $Revision: 1.1 $
-%     $Date: 2013/12/11 00:49:08 $
-%
-
-%-------------------------------------------------------------------
-% VERSION INFORMATION:
-%
-%   $RCSfile: epoch2serial.m,v $
-%   $Revision: 1.1 $
-%   $Date: 2013/12/11 00:49:08 $
-%
-%   $Log: epoch2serial.m,v $
-%   Revision 1.1  2013/12/11 00:49:08  cflynn
-%   Committing anctools
-%
-%   Revision 1.1  2011/04/08 13:49:29  cflynn
-%   *** empty log message ***
-%
-%   Revision 1.2  2008/10/20 17:08:50  cflynn
-%   Force serial time as double irrespective of incoming time fields in netcdf file.  This was done to get around data type limitation of datenum.  Seems that there is no reason datanum shouldn't eat other numeric data types, but there ya go.
-%
-%   Revision 1.1  2007/11/09 01:24:20  cflynn
-%   This repository is for Matlab m-files
-%
-%   Revision 1.1  2007/09/05 08:08:56  cflynn
-%   Creating Matlab Library for central ARM distribution of matlab scripts and functions
-%
-%   Revision 1.5  2006/06/19 18:43:01  sbeus
-%   Added back CVS log information.
-%
+% Converts igor time (seconds since Jan 1, 1904) to serial time (days since Jan 1, 0 AD).
 %
 %-------------------------------------------------------------------
  
@@ -41,5 +10,4 @@ SEC_PER_DAY  = 86400;
 
 index = (igor_time) ./ SEC_PER_DAY;
 serial = double(index + DAYS_TO_1904);
-disp(' igor2serial does not quite work.  Seems to be off by ~15 minutes')
 return
