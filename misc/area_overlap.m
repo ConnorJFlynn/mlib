8dfa3def8b87cce3b0c2ddd274dec899a6555d83
ConnorJFlynn <Connor.Flynn@pnnl.gov>
2016-10-17 16:12:49 -0700

    area of overlapping circles
    Used with 4STAR and 2STAR to explore effect of vignetting apertures on
    view of solar disk

---
+++ misc/area_overlap.m
@@ -0,0 +1,12 @@
+function A = area_overlap(r, R, d)
+
+d = abs(d);
+
+part_1 = r.^2 .* (acos((d.^2 + r.^2 -R.^2)./(2.*d.*r)));
+part_2 = R.^2 .* (acos((d.^2 + R.^2 -r.^2)./(2.*d.*R)));
+part_3 = -0.5.*sqrt((r+R-d).*(d+r-R).*(d+R-r).*(d+r+R));
+
+A = part_1 + part_2 + part_3;
+A = A./ (pi.*r.^2);
+
+return
