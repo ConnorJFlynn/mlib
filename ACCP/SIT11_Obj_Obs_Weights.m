a4b9cd071c39cc14c2bea07b03153b6f605e3c55
ConnorJFlynn <conjo.flynn@gmail.com>
2022-03-03 13:54:59 -0600

    Create SIT11_Obj_Obs_Weights.m
    Maybe unnecessary since Arlindo did the averaging himself instead.

---
+++ ACCP/SIT11_Obj_Obs_Weights.m
@@ -0,0 +1,15 @@
+function OOW = SIT11_Obj_Obs_Weights
+% Drawn from 20200921 - Science Benefit Sccores missing highlighted.xlsm
+% References O3 da Silva, Braun - SATM Rel F
+
+% 4 Obj x 3Obs
+% [NaN,NaD,OND  x O5,O6,O7,O8]
+OOW = [ .43, .42, .15; ...
+      .4 ,  .4, .2 ; ...
+      .7,   .1,  .2; ...
+      .8,   .1,  .1];
+
+end
+
+
+
