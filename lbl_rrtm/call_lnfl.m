64b5f844b78839f73cad819087c667dfad2701e6
ConnorJFlynn <conjo.flynn@gmail.com>
2022-06-20 16:35:37 -0500

    Update call_lnfl.m
    call_lnfl not complete. Calls lnfl but doesn't conduct post-run maintenance yet. This is important, especially as it is the template for call_lblrtm.

--- lbl_rrtm/call_lnfl.m
+++ lbl_rrtm/call_lnfl.m
@@ -1,15 +1,40 @@
-function tape3_fullname = call_lnfl(lnfl_tape5,stem)
-% tape3_fullname = call_lnfl(lnfl_tape5)
+function tape3_fullname = call_lnfl(lnfl_tape5)
+% tape3_fullname = call_lnfl(lnfl_tape5,tag)
 % Accepts or queries for lnfl_tape5.
-% if isavar(stem), appends stem to supplied/selected lnfl_tape5
+% if isavar(tag), appends tag to supplied/selected lnfl_tape5
 % if LNFL tape5 exists, deletes.
 % Copies lnfl_tape5 to lnfl_path/TAPE5
 % Calls LNFL
-% Copies TAPE3 to ['TAPE3.' stem] (moves instead? yes, to avoid inadvertent
-% use of unknown TAPE3 output)             
+% Copies TAPE3 to ['TAPE3.' tag] (moves instead? yes, to avoid inadvertent
+% use of unknown TAPE3 output)
 
-
-
-
+lnfl_path = getpname('lnfl');
+if isafile('lnfl_exe.mat')
+    lnfl_exe = load('lnfl_exe.mat');
+else
+    lnfl_exe.fname = getfullname('lnfl*','lnfl','Select lnfl executable.');
+    save('lnfl_exe.mat','-struct','lnfl_exe');
+end
+if ~isafile(lnfl_exe.fname)
+    lnfl_exe.fname = getfullname('lnfl*','lnfl','Select lnfl executable.');
+    save('lnfl_exe.mat','-struct','lnfl_exe');
+end
+if ~isavar('lnfl_tape5')||~isfile(lnfl_tape5)
+    lnfl_tape5 = getfullname('*TAPE5*','lnfl_tape5');
+end
+[~,tape5,ext]= fileparts(lnfl_tape5); 
+tape5 = [tape5, ext];
+[tape5, tag] = strtok(tape5,'.');
+if ~isempty(tag)
+    tag = tag(2:end);
+else
+    tag = input('Enter a descriptive tag for this lnfl TAPE5 file: ', 's')
+end
+if isafile([lnfl_path,'TAPE5'])
+    delete([lnfl_path,'TAPE5']);
+end
+copyfile(lnfl_tape5,[lnfl_path,'TAPE5']);
+tic; status = system(lnfl_exe.fname)
+toc
 
 return
