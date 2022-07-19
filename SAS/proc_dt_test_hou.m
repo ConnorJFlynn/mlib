3e4429a55bccbf8e0c3f4a2b6c143390e108fd9b
ConnorJFlynn <conjo.flynn@gmail.com>
2022-07-18 22:04:09 -0500

    Create proc_dt_test_hou.m

---
+++ SAS/proc_dt_test_hou.m
@@ -0,0 +1,173 @@
+function proc_dt_test 
+zenir = rd_SAS_raw(getfullname('*sasze*nir*.csv'));
+zevis = rd_SAS_raw(getfullname('*sasze*vis*.csv'));
+% This is for second test for HOU as teh first didn't work quite right bcuz nir tint hi was 450 ms
+% which was longer than the 0.4 sec period, so filled with -9999
+vlo = 2; vhi = 15;
+dark_ = zevis.Shutter_open_TF==0; 
+dark_(1:end-1) = dark_(1:end-1)&dark_(2:end); dark_(2:end) = dark_(1:end-1)&dark_(2:end);
+dark_lo = dark_ & zevis.t_int_ms==vlo; 
+dark_hi = dark_ & zevis.t_int_ms==vhi;
+lite_ = zevis.Shutter_open_TF==1; 
+lite_(1:end-1) = lite_(1:end-1) & lite_(2:end); lite_(2:end) = lite_(1:end-1) & lite_(2:end);
+lite_lo = lite_ & zevis.t_int_ms==vlo; 
+lite_hi = lite_ & zevis.t_int_ms==vhi;
+
+lo_darks = sum(dark_lo(1:end-1)&~dark_lo(2:end)); 
+lo_darks_i = find(dark_lo);
+lo_dark.time = zeros([lo_darks,1]);
+lo_dark.vspec = zeros([lo_darks,length(zevis.wl)]);
+lo_dark.nspec = zeros([lo_darks,length(zenir.wl)]);
+m = lo_darks_i(1); q = 0;p = 1;n = 1;
+dtime = zenir.time(lo_darks_i(n));
+vdark = zevis.spec(lo_darks_i(n),:);
+ndark = zenir.spec(lo_darks_i(n),:);
+for n = 2:length(lo_darks_i)
+   if lo_darks_i(n)-m == 1
+      dtime = dtime + zenir.time(lo_darks_i(n));
+      vdark = vdark + zevis.spec(lo_darks_i(n),:);
+      ndark = ndark + zenir.spec(lo_darks_i(n),:);
+      p = p +1;
+   else
+      q = q+1;
+      lo_dark.time(q) = dtime./p;
+      lo_dark.nspec(q,:) = ndark./p;
+      lo_dark.vspec(q,:) = vdark./p;
+      p = 1;
+      dtime = zenir.time(lo_darks_i(n));
+      vdark = zevis.spec(lo_darks_i(n),:);
+      ndark = zenir.spec(lo_darks_i(n),:);
+   end
+   m = lo_darks_i(n);
+end
+
+hi_darks = sum(dark_hi(1:end-1)&~dark_hi(2:end)); 
+hi_darks_i = find(dark_hi);
+hi_dark.time = zeros([hi_darks,1]);
+hi_dark.vspec = zeros([hi_darks,length(zevis.wl)]);
+hi_dark.nspec = zeros([hi_darks,length(zenir.wl)]);
+m = hi_darks_i(1); q = 0;p = 1;n = 1;
+dtime = zenir.time(hi_darks_i(n));
+vdark = zevis.spec(hi_darks_i(n),:);
+ndark = zenir.spec(hi_darks_i(n),:);
+for n = 2:length(hi_darks_i)
+   if hi_darks_i(n)-m == 1
+      dtime = dtime + zenir.time(hi_darks_i(n));
+      vdark = vdark + zevis.spec(hi_darks_i(n),:);
+      ndark = ndark + zenir.spec(hi_darks_i(n),:);
+      p = p +1;
+   else
+      q = q+1;
+      hi_dark.time(q) = dtime./p;
+      hi_dark.nspec(q,:) = ndark./p;
+      hi_dark.vspec(q,:) = vdark./p;
+      p = 1;
+      dtime = zenir.time(hi_darks_i(n));
+      vdark = zevis.spec(hi_darks_i(n),:);
+      ndark = zenir.spec(hi_darks_i(n),:);
+   end
+   m = hi_darks_i(n);
+end
+
+zenir.darks = zeros(size(zenir.spec));
+for vw = length(zenir.wl):-1:1
+   intime = zenir.time(lite_hi);
+   tmp = interp1(hi_dark.time, hi_dark.nspec(:,vw), intime,'linear'); nans = isnan(tmp);
+   tmp(nans) = interp1(hi_dark.time, hi_dark.nspec(:,vw), intime(nans),'nearest','extrap');
+   zenir.darks(lite_hi,vw) =tmp;
+
+   intime = zenir.time(lite_lo);
+   tmp = interp1(lo_dark.time, lo_dark.nspec(:,vw), intime,'linear'); nans = isnan(tmp);
+   tmp(nans) = interp1(lo_dark.time, lo_dark.nspec(:,vw), intime(nans),'nearest','extrap');
+   zenir.darks(lite_lo,vw) =tmp;
+   
+end
+
+zevis.darks = zeros(size(zevis.spec));
+for vw = length(zevis.wl):-1:1
+   intime = zenir.time(lite_hi);
+   tmp = interp1(hi_dark.time, hi_dark.vspec(:,vw), intime,'linear'); nans = isnan(tmp);
+   tmp(nans) = interp1(hi_dark.time, hi_dark.vspec(:,vw), intime(nans),'nearest','extrap');
+   zevis.darks(lite_hi,vw) =tmp;
+
+   intime = zevis.time(lite_lo);
+   tmp = interp1(lo_dark.time, lo_dark.vspec(:,vw), intime,'linear'); nans = isnan(tmp);
+   tmp(nans) = interp1(lo_dark.time, lo_dark.vspec(:,vw), intime(nans),'nearest','extrap');
+   zevis.darks(lite_lo,vw) =tmp;
+   
+end
+WD = 2.^16;
+zevis.wd = zevis.spec./WD; zevis.sig = zevis.spec - zevis.darks; 
+zevis.rate(zevis.t_int_ms==vlo,:) = zevis.sig(zevis.t_int_ms==vlo,:)./unique(zevis.t_int_ms(zevis.t_int_ms==vlo));
+zevis.rate(zevis.t_int_ms==vhi,:) = zevis.sig(zevis.t_int_ms==vhi,:)./unique(zevis.t_int_ms(zevis.t_int_ms==vhi));
+
+zenir.wd = zenir.spec./WD; zenir.sig = zenir.spec - zenir.darks; 
+zenir.rate(zevis.t_int_ms==vlo,:) = zenir.sig(zevis.t_int_ms==vlo,:)./unique(zenir.t_int_ms(zevis.t_int_ms==vlo));
+zenir.rate(zevis.t_int_ms==vhi,:) = zenir.sig(zevis.t_int_ms==vhi,:)./unique(zenir.t_int_ms(zevis.t_int_ms==vhi));
+
+
+
+
+
+figure; plot(zenir.wl, max(zenir.rate(zevis.t_int_ms==vlo,:)),'r-')
+xl = xlim; 
+xl_ = zenir.wl>xl(1) & zenir.wl<xl(2);
+
+shut_ = zevis.Shutter_open_TF==0; 
+shut_(1:end-1) = shut_(1:end-1)|shut_(2:end); shut_(2:end) = shut_(1:end-1)|shut_(2:end);
+zenir.rate(:,~xl_) = NaN;zenir.rate(shut_,:) = NaN;
+zevis.rate(shut_,~xl_) = NaN;
+zevis.rate(zevis.spec==2.^16) = NaN;
+zenir.rate(zenir.spec==2.^16) = NaN;
+nrate_hi = zenir.rate(zevis.t_int_ms==vhi,xl_);
+ntemp_hi = zenir.Temp_chiller_C(zevis.t_int_ms==vhi);
+nrate_lo = zenir.rate(zevis.t_int_ms==vlo,xl_);
+wd_ni = zenir.wd(zevis.t_int_ms==vhi,xl_);
+wd_ni = wd_ni(:);
+wd_no = zenir.wd(zevis.t_int_ms==vlo,xl_);
+wd_no = wd_no(:);
+
+
+
+vrate_hi = zevis.rate(zevis.t_int_ms==vhi,xl_);
+vrate_lo = zevis.rate(zevis.t_int_ms==vlo,xl_);
+wd_hi = zevis.wd(zevis.t_int_ms==vhi,xl_);
+wd_hi = wd_hi(:);
+wd_lo = zevis.wd(zevis.t_int_ms==vlo,xl_);
+wd_lo = wd_lo(:);
+figure; plot(zevis.time(zevis.t_int_ms==vhi),vrate_hi(:,300),'.',zevis.time(zevis.t_int_ms==vlo),vrate_lo(:,300),'.')
+
+vrate_rat = vrate_lo(:)./vrate_hi(:);
+figure; plot(wd_hi(:),vrate_rat,'.'); logy
+v = axis;
+wd = [0:.01:1]; wd(end) = wd(end) -.005;wd(1) = wd(1) +.005;
+for wd_i = length(wd):-1:2
+    sub = wd_hi>wd(wd_i-1) & wd_hi<=wd(wd_i) & vrate_rat>(v(3))&vrate_rat<(v(4));
+    vrat(wd_i) = nanmean(vrate_rat(sub));
+end
+wd(1) = []; vrat(1) = [];
+figure; plot(wd, vrat,'o-r');
+xlabel('Pixel Well Depth');
+ylabel('Low Gain Rate / High Gain Rate')
+title({'HOU SASZe VIS Dual Rate Test (2 ms / 15 ms)';[datestr(zenir.time(1),'yyyy-mm-dd'),' ~4 hours spanning noon']})
+
+figure; plot(zenir.time(zevis.t_int_ms==vhi),nrate_hi(:,100),'.',zenir.time(zevis.t_int_ms==vlo),nrate_lo(:,100),'.')
+nrate_rat = nrate_lo(:)./nrate_hi(:);
+ntemp = ntemp_hi.*ones([1,222]);ntemp = ntemp(:);
+figure; plot(wd_ni(:),nrate_rat,'.'); logy
+figure; scatter(fliplr(wd_ni(1:10:end)), fliplr(nrate_rat(1:10:end)), 2, (ntemp(1:10:end))); axis(v)
+v = axis;
+wd = [0:.05:1]; wd(end) = wd(end) -.005;wd(1) = wd(1) +.005;
+for wd_i = length(wd):-1:2
+    sub = wd_ni>wd(wd_i-1) & wd_ni<=wd(wd_i) & nrate_rat>(v(3))&nrate_rat<(v(4));
+    nrat(wd_i) = nanmean(nrate_rat(sub));
+end
+wd(1) = []; nrat(1) = [];
+figure; plot(wd, nrat,'o-r');
+xlabel('Pixel Well Depth');
+ylabel('Low Gain Rate / High Gain Rate')
+title({'HOU SASZe NIR Dual Rate Test (100 ms / 400 ms)';[datestr(zenir.time(1),'yyyy-mm-dd'),' ~4 hours spanning noon']})
+
+
+
+return
