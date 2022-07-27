fef9ff91073838dfec07580d85b00bfbaed3e4a8
ConnorJFlynn <conjo.flynn@gmail.com>
2022-07-27 16:05:42 -0500

    Update popsb1_to_dNdlogDp.m

--- aaf/popsb1_to_dNdlogDp.m
+++ aaf/popsb1_to_dNdlogDp.m
@@ -21,10 +21,16 @@ for t = length(pops.time):-1:1
 end
 okk = pops.vdata.alt>0 & ext>0;
 figure; plot(ext(okk),pops.vdata.alt(okk),'c*');
-figure; scatter(pops.time(okk), pops.vdata.alt(okk), 12, ext(okk))
-
-mplext = load(getfullname(['*mplkext*',datestr(pops.time(1),'yyyymmdd'),'*.mat'],'mpl_kext'));
-
-mplext_on_fltpath = timehtcurtain2rawpopsextfltlvl(mpl, pops);
+figure; scatter(pops.time(okk), pops.vdata.alt(okk), 12, ext(okk)); dynamicDateTicks; 
+ylabel('Altitude [m AGL]'); xlabel('time [UT]');
+title({'POPS-derived extinction and flight profile';...
+   [datestr(pops.time(1),'yyyy-mm-dd HH:MM-'),datestr(pops.time(end),'HH:MM')]});
+cb = colorbar; set(get(cb,'title'),'string',{'B_E_x_t';'[1/Mm]'});
+[pname, fname] = fileparts(pops.fname); pname = [pname, filesep];
+fname = [fname, '.Ext_and_Flight'];
+saveas(gcf, [pname, fname, '.png']);saveas(gcf, [pname, fname, '.fig']);
+% mplext = load(getfullname(['*mplkext*',datestr(pops.time(1),'yyyymmdd'),'*.mat'],'mpl_kext'));
+% 
+% mplext_on_fltpath = timehtcurtain2rawpopsextfltlvl(mpl, pops);
 
 end
