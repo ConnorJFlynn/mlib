92c1321d1c1c1754fb901de5b48001af2df9e778
ConnorJFlynn <conjo.flynn@gmail.com>
2022-08-02 10:34:04 -0500

    Update timehtcurtain2fltlvl.m

--- aaf/timehtcurtain2fltlvl.m
+++ aaf/timehtcurtain2fltlvl.m
@@ -1,21 +1,25 @@
-function out = timehtcurtain2fltlvl(mpl, pops)
-
+function ants = timehtcurtain2fltlvl(mpl, pops)
+if ~isavar('mpl')||isempty(mpl)
+    mpl = anc_load(getfullname('*mpl*kext*.mat','kext'));
+end
 % alpha_a is transpose so size(dim1) == length(mpl.time) for interp1
 ext_curtain = interp1(mpl.time, mpl.klett.alpha_a', pops.time, 'linear')';
 
-bin = interp1(mpl.range(1:1001),[1:1001],pops.vdata.alt./1000, 'nearest');
+bin = interp1(mpl.range(1:1001),[1:1001],pops.vdata.alt./1000, 'nearest','extrap');
 
-out = real(ext_curtain(sub2ind(size(ext_curtain),bin', 1:length(bin))));
+ants = real(ext_curtain(sub2ind(size(ext_curtain),bin, 1:length(bin))));
 
 % % This also works...
 % for t = length(aafnav.time):-1:1
 %    out(t) = real(ext_curtain(bin(t),t));
 % end
-figure; scatter(serial2Hh(pops.time), pop.vdata.alt./1000, 12, out)
+miss = pops.vdata.alt < 0; pops.vdata.alt(miss) = NaN;
+figure; scatter((pops.time), pops.vdata.alt./1000, 12, ants); dynamicDateTicks;
+cb = colorbar; set(get(cb,'title'),'string',{'B_e_x_t [1/km]'}); 
 
-figure; plot(popsext.ext_um./1e3, pops.vdata.alt./1000,'x'); xlabel('ext'); ylabel('flight alt (km)');
+figure; plot(popsext.ext_um./1e3, pops.vdata.alt,'x'); xlabel('ext'); ylabel('flight alt (m AGL)');
 title('MPL extinction profile mapped to AAF U2 flight for July 9, 2022')
 
-out = timehtcurtain2fltlvl(mpl, popsext)
+
 
 end
