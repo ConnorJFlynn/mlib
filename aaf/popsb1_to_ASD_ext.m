a6029792c48a334c79947b1c15259cdb65984d15
ConnorJFlynn <conjo.flynn@gmail.com>
2022-08-03 15:33:21 -0500

    AAF POPS Ext

--- aaf/popsb1_to_ASD_ext.m
+++ aaf/popsb1_to_ASD_ext.m
@@ -1,4 +1,4 @@
-function [dNdlogDp,Dp, pops,dlogDp] = popsb1_to_dNdlogDp(pops)
+function [ASD] = popsb1_to_ASD_ext(pops)
 %[dNdlogDp] = read_pops_b1(pops)
 % Reads aaf pops b1 file.  Converts raw measurements to dN and dNdlogDp
 if ~isavar('pops')||~isafile(pops)
@@ -7,21 +7,23 @@ end
 
 SD = [pops.vdata.dn_135_150;pops.vdata.dn_150_170;pops.vdata.dn_170_195; pops.vdata.dn_195_220; pops.vdata.dn_220_260; pops.vdata.dn_260_335; pops.vdata.dn_335_510;pops.vdata.dn_510_705; pops.vdata.dn_705_1380; pops.vdata.dn_1380_1760; pops.vdata.dn_1760_2550; pops.vdata.dn_2550_3615];
 bounds = [135,150,170,195,220,260,335,510, 705, 1380, 1760, 2550, 3615]';
-Dp12 = mean([bounds(1:end-1), bounds(2:end)],2);
-dlogDp12 = log10(bounds(2:end)./bounds(1:end-1)); % log10
-dNdlogDp12 = SD ./ (dlogDp * ones(size(pops.time)));
-
+Dp = mean([bounds(1:end-1), bounds(2:end)],2);
+dlogDp = log10(bounds(2:end)./bounds(1:end-1)); % log10
+dNdlogDp = SD ./ (dlogDp * ones(size(pops.time)));
+tic
 for t = length(pops.time):-1:1
     ok = dNdlogDp(:,t)>=0; ok(1) = false;
     if any(ok)
         retval = SizeDist_Optics(1.52, Dp(ok), dNdlogDp(ok,t), 532, 'normalized',false,'nobackscat',true);
-        ext(t) = retval.extinction;
+        ext_532(t) = retval.extinction;
+        retval = SizeDist_Optics(1.52, Dp(ok), dNdlogDp(ok,t), 355, 'normalized',false,'nobackscat',true);
+        ext_355(t) = retval.extinction;
     end
-    t
 end
-okk = pops.vdata.alt>0 & ext>0;
-figure; plot(ext(okk),pops.vdata.alt(okk),'c*');
-figure; scatter(pops.time(okk), pops.vdata.alt(okk), 12, ext(okk)); dynamicDateTicks; 
+toc
+okk = pops.vdata.alt>0 & ext_532>0;
+figure; plot(ext_355(okk),pops.vdata.alt(okk),'*',ext_532(okk),pops.vdata.alt(okk),'x');
+figure; scatter(pops.time(okk), pops.vdata.alt(okk), 12, ext_532(okk)); dynamicDateTicks; 
 ylabel('Altitude [m AGL]'); xlabel('time [UT]');
 title({'POPS-derived extinction and flight profile';...
    [datestr(pops.time(1),'yyyy-mm-dd HH:MM-'),datestr(pops.time(end),'HH:MM')]});
@@ -32,5 +34,11 @@ saveas(gcf, [pname, fname, '.png']);saveas(gcf, [pname, fname, '.fig']);
 % mplext = load(getfullname(['*mplkext*',datestr(pops.time(1),'yyyymmdd'),'*.mat'],'mpl_kext'));
 % 
 % mplext_on_fltpath = timehtcurtain2rawpopsextfltlvl(mpl, pops);
-
+ASD.time = pops.time;
+ASD.Alt_mAGL = pops.vdata.alt;
+ASD.dP = Dp;
+ASD.dlogDp = dlogDp;
+ASD.dNdlogDp = dNdlogDp;
+ASD.ext_355 = ext_355;
+ASD.ext_532 = ext_532;
 end
--- aaf/timehtcurtain2rawpopsextfltlvl.m
+++ aaf/timehtcurtain2rawpopsextfltlvl.m
@@ -1,9 +1,9 @@
-function out = timehtcurtain2rawpopsextfltlvl(mpl, popsext)
+function out = timehtcurtain2rawpopsextfltlvl(mpl, ASD)
 
 % alpha_a is transpose so size(dim1) == length(mpl.time) for interp1
-ext_curtain = interp1(mpl.time, mpl.klett.alpha_a', popsext.time, 'linear')';
+ext_curtain = interp1(mpl.time, mpl.klett.alpha_a', ASD.time, 'linear')';
 
-bin = interp1(mpl.range(1:1001),[1:1001],popsext.alt_mAGL./1000, 'nearest')';
+bin = interp1(mpl.range(1:1001),[1:1001],ASD.alt_m_AGL./1000, 'nearest')';
 
 out = real(ext_curtain(sub2ind(size(ext_curtain),bin, 1:length(bin))));
 
@@ -11,12 +11,12 @@ out = real(ext_curtain(sub2ind(size(ext_curtain),bin, 1:length(bin))));
 % for t = length(aafnav.time):-1:1
 %    out(t) = real(ext_curtain(bin(t),t));
 % end
-figure; scatter(serial2Hh(popsext.time), popsext.alt_mAGL./1000, 12,out)
+figure; scatter(serial2Hh(popsext.time), ASD.alt_m_AGL./1000, 12,out)
 
-figure; plot(out, popsext.alt_mAGL./1000,'x'); xlabel('ext'); ylabel('flight alt (km)');
+figure; plot(out, ASD.alt_m_AGL./1000,'x'); xlabel('ext'); ylabel('flight alt (km)');
 title('MPL extinction profile mapped to AAF U2 flight for July 9, 2022')
 hold('on')
-plot(popsext.ext_um./1000, popsext.alt_mAGL./1000,'ro');
-figure; plot(nanmean(mpl.klett.alpha_a(:,mpl.time>popsext.time(1)&mpl.time<popsext.time(end)),2), mpl.range(1:1001),'kx')
+plot(ASD.ext_532, ASD.alt_mAGL./1000,'ro');
+figure; plot(nanmean(mpl.klett.alpha_a(:,mpl.time>ASD.time(1)&mpl.time<ASD.time(end)),2), mpl.range(1:1001),'kx')
 
 end
