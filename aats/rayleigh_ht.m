60c1a6250a885ae928890fbbe928f0c68c1fdf8c
ConnorJFlynn <conjo.flynn@gmail.com>
2022-10-03 11:55:51 -0500

    Update rayleigh_ht.m
    Handle no supplied pressure

--- aats/rayleigh_ht.m
+++ aats/rayleigh_ht.m
@@ -2,6 +2,10 @@ function tau_r=rayleigh_ht(lambda,press)
 %Computes Rayleigh Scattering according to Hansen and Ttavis, 1974 Space Sci. rev. 527-610
 %lambda has to be in microns, pressure in hPa
 
+if ~isavar('press')||isempty(press)
+    press = 1013.25;
+end
+
 A=0.008569;
 B=0.0113;
 C=0.00013;
