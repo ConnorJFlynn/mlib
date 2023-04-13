a2065d210a3d5fa962fb43795129db0d554674fd
ConnorJFlynn <conjo.flynn@gmail.com>
2023-04-13 16:15:26 -0500

    Update rbifit.m
    Now pass in existing fig handle if desired, and pass out P_bar.  Also generate plot even if no values excluded.

--- lin_al/rbifit.m
+++ lin_al/rbifit.m
@@ -1,4 +1,4 @@
-function good = rbifit(X,Y,M,pct,good);
+function [good, P_bar] = rbifit(X,Y,M,pct,good,fig);
 %good = rbifit(X,Y,M,pct,good);
 % compute a best line bifit
 % compute the mean absolute deviation from this best line
@@ -7,6 +7,7 @@ function good = rbifit(X,Y,M,pct,good);
 % Iterate until no points removed?
 if ~isavar('good')
    good = true(size(X));
+   good = ~isnan(X)&~isnan(Y);
 end
    N = 1;
 if ~isavar('M')
@@ -20,11 +21,18 @@ end
 val = polyval(P_bar,X);
 AD = abs(val - Y);
 MAD = mean(AD(good>0));
-new_good = (AD < (M.*MAD));
-if (sum(good)-sum(new_good))/sum(good) > (pct./100)
-   figure_(17); plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'rx',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
+new_good = (AD < (M.*MAD))&good;
+   if isavar('fig')&&isgraphics(fig)
+      figure_(fig);
+   else
+      figure;
+   end
+   plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'rx',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
+ 
+if abs(sum(good)-sum(new_good))/sum(good) > (pct./100)
+   plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'rx',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
    title(['Keep(',num2str(sum(new_good)),'), Remove(',sprintf('%2.1g%%',100.*(sum(good)-sum(new_good))./sum(good)),')'])
-   good = rbifit(X,Y,M,pct,new_good);
+   good = rbifit(X,Y,M,pct,new_good,gcf);
 end
 % compute a best line fit
 % compute the mean absolute deviation from this best line
