0eab204e7cd4e0f65a0d31a9128b7e6150eb4c03
ConnorJFlynn <conjo.flynn@gmail.com>
2023-07-25 11:02:33 -0500

    Update rbifit.m

--- lin_al/rbifit.m
+++ lin_al/rbifit.m
@@ -5,7 +5,7 @@ function [good, P_bar] = rbifit(X,Y,M,pct,good,fig);
 % re-compute best line fit excluding points with absolute deviation
 % greater than 6 MAD.
 % Iterate until no points removed?
-if ~isavar('good')
+if ~isavar('good')||isempty(good)
    good = true(size(X));
    good = ~isnan(X)&~isnan(Y);
 end
@@ -32,7 +32,7 @@ new_good = (AD < (M.*MAD))&good;
 if abs(sum(good)-sum(new_good))/sum(good) > (pct./100)
    plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'r.',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
    title(['Keep(',num2str(sum(new_good)),'), Remove(',sprintf('%2.1g%%',100.*(sum(good)-sum(new_good))./sum(good)),')'])
-   good = rbifit(X,Y,M,pct,new_good,gcf);
+   [good, P_bar] = rbifit(X,Y,M,pct,new_good,gcf);
 end
 % compute a best line fit
 % compute the mean absolute deviation from this best line
