cb585ae8ee1c2905b354be3660692fc17186a6ae
ConnorJFlynn <conjo.flynn@gmail.com>
2023-08-22 22:47:57 -0500

    Update Pol_Ze_no_45.m

--- rad_cals/Pol_Ze_no_45.m
+++ rad_cals/Pol_Ze_no_45.m
@@ -23,8 +23,9 @@ if any(pol.nm>1500) % Then it is NIR
 else
  bad_wl = pol.nm<550 | pol.nm>950;
 end
-Tr(:,bad_wl) = NaN;
-figure; lines = plot([0:20:360],Tr,'-'); colormap('jet'); recolor(lines,pol.nm); title(exp_name)
+Tr(:,bad_wl) = NaN; meanTr = nanmean(Tr);Tr = Tr - ones([size(Tr,1),1])*meanTr;
+figure; lines = plot([0:20:360],Tr,'o'); colormap('jet'); recolor(lines,pol.nm); title(exp_name);
+figure; lines = plot([0:20:360],fliplr(Tr),'o'); colormap('jet'); recolor(lines,flipud(pol.nm)); title(exp_name)
 % figure; lines = plot(pol.nm, Tr','-'); colormap(jej); recolor(lines,[0:20:360]); title(exp_name)
 
 % This block of code removes the dependence of a 360 degree rotation due to
