c0cf44f2cc1961fd0b6b86c817379b385839b1c9
ConnorJFlynn <conjo.flynn@gmail.com>
2023-09-07 23:50:09 -0500

    Avoid plotting if no fig passed in
    Not pretty but it works.

--- lin_al/rbifit.m
+++ lin_al/rbifit.m
@@ -24,16 +24,22 @@ MAD = mean(AD(good>0));
 new_good = (AD < (M.*MAD))&good;
    if isavar('fig')&&isgraphics(fig)
       figure_(fig);
-   else
+   elseif isavar('fig')&&isempty(fig)
       figure;
    end
+   if isavar('fig')
    plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'r.',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
- 
+   end
 if abs(sum(good)-sum(new_good))/sum(good) > (pct./100)
+   if isavar('fig')
    plot(X(new_good), Y(new_good), 'k.',X(good), Y(good), 'r.',[0,max(X)], [0,max(X)],'k--',  [0,max(X)], polyval(P_bar,[0,max(X)]),'b-'); 
    title(['Keep(',num2str(sum(new_good)),'), Remove(',sprintf('%2.1g%%',100.*(sum(good)-sum(new_good))./sum(good)),')'])
-   [good, P_bar] = rbifit(X,Y,M,pct,new_good,gcf);
+   [good, P_bar] = rbifit(X,Y,M,pct,new_good,fig);
+   else
+   [good, P_bar] = rbifit(X,Y,M,pct,new_good);   
+   end
 end
+
 % compute a best line fit
 % compute the mean absolute deviation from this best line
 % re-compute best line fit excluding points with absolute deviation
