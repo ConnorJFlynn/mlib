1fdc4840a2e43ccad2f64909c1e4cc2f5b02b454
ConnorJFlynn <conjo.flynn@gmail.com>
2023-09-07 23:50:36 -0500

    Update den2plot.m
    A bit faster, factor of 2 or so

--- plotting/den2plot.m
+++ plotting/den2plot.m
@@ -1,26 +1,33 @@
 function D = den2plot(X,Y,d)
 N = length(X);
 ds = zeros(N,N);XX = ds; YY = ds;
+dds = ds;
 X = X-min(X); Y = Y-min(Y);
 X = X./max(X); Y = Y./max(Y);
-% xX = [X;X]; yY = [Y;Y];
-% for n = 1:N
-%   XX(:,n) = xX(n:(N+n-1)); 
-%   YY(:,n) = yY(n:(N+n-1));
-%   ds(:,n) = sqrt((XX(:,n)-XX(:,1)).^2 +   (YY(:,n)-YY(:,1)).^2);
-% end
 
-for i = 1:N
-   for j = (i+1):N
-      ds(i,j) = sqrt((X(i)-X(j)).^2+(Y(i)-Y(j)).^2);
-      ds(j,i) = ds(i,j);
-   end
+% tic
+% for i = 1:N
+%    for j = (i+1):N
+%       ds(i,j) = sqrt((X(i)-X(j)).^2+(Y(i)-Y(j)).^2);
+%       ds(j,i) = ds(i,j);
+%    end
+% end
+% toc
+% tic
+% This is faster but computationally less efficient in that it does not take
+% advantage of the diagonal symmetry ds(i,j) == ds(j,i)
+xX = [X;X]; yY = [Y;Y];
+for n = N:-1:1
+  XX(:,n) = xX(n:(N+n-1)); 
+  YY(:,n) = yY(n:(N+n-1));
+  dds(:,n) = sqrt((XX(:,n)-XX(:,1)).^2 +   (YY(:,n)-YY(:,1)).^2);
 end
+% toc
 if ~isavar('d')
    d = 0.01;
 end
-D = sum(ds<d);
-
+% D = sum(ds<d);
+Dd = sum(dds<d,2);
 end
 
 
