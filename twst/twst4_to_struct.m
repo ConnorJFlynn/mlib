fc87237ea9b3462f6b369078cdddb267a2ae8dbe
ConnorJFlynn <conjo.flynn@gmail.com>
2023-12-26 14:31:41 -0600

    Update twst4_to_struct.m
    updated to query group ID vs names

--- twst/twst4_to_struct.m
+++ twst/twst4_to_struct.m
@@ -8,20 +8,32 @@ end
 
 ncid = netcdf.open(in_file);
 Grp = netcdf.inqGrps(ncid);
+for g=length(Grp):-1:1
+   GrpName(g) = {netcdf.inqGrpName(Grp(g))};
+end
+
 % Grp(1) level 0 data
 % netcdf.inqGrpName(Grp(2));
-lv0_id = Grp(1); % level_0
-lv1_id = Grp(2); % level_1 
+lv0_id = Grp(foundstr(GrpName,'level_0')); % level_0
+lv1_id = Grp(foundstr(GrpName,'level_1')); % level_1 
+cal_id = Grp(foundstr(GrpName,'calibration'));
 cod_id = netcdf.inqVarID(lv1_id,'cod');
 twst.cod = netcdf.getVar(lv1_id,cod_id)';
 netcdf.inqVarID(lv0_id,'wavelength_vis');
 lev0_grps = netcdf.inqGrps(Grp(1));
-netcdf.inqGrpName(lev0_grps(2)) ; % sky
-sky_id =lev0_grps(2); 
-dark_id = lev0_grps(1);
-cal_id = Grp(6);
-cal_grps =netcdf.inqGrps(cal_id);
-rad_cal_id = cal_grps(2);
+for g = length(lev0_grps):-1:1
+   lev0_Name(g) = {netcdf.inqGrpName(lev0_grps(g))};
+end
+
+sky_id =lev0_grps(foundstr(lev0_Name,'sky')); 
+dark_id = lev0_grps(foundstr(lev0_Name,'dark'));
+
+
+Grp = netcdf.inqGrps(cal_id);
+for g=length(Grp):-1:1
+   GrpName(g) = {netcdf.inqGrpName(Grp(g))};
+end
+rad_cal_id = Grp(foundstr(GrpName,'radiance_cal'));
 rad_cal_grps = netcdf.inqVarID(rad_cal_id,'vis');
 [lv0_ndims,lv0_nvars,lv0_ngatts,lv0_unlimdimid] = netcdf.inq(lv0_id);
 twst.wl_B = (netcdf.getVar(lv0_id,netcdf.inqVarID(lv0_id,'wavelength_nir')));
@@ -42,12 +54,13 @@ twst.time = epoch2serial(twst.epoch);
 twst.dark_epoch = netcdf.getVar(dark_id,netcdf.inqVarID(dark_id,'time'))';
 twst.dark_time = epoch2serial(twst.dark_epoch);
 dt_i = floor(interp1(twst.dark_epoch, [1:length(twst.dark_epoch)],twst.epoch,'linear','extrap'));
+dt_i(dt_i<1) = 1; dt_i(dt_i>length(twst.dark_time)) = length(twst.dark_time);
 darks_A = twst.dark_A(:,dt_i);
 darks_B = twst.dark_B(:,dt_i);
 twst.raw_A = twst.zenrad_A ./ (twst.cal_A*ones([1,size(twst.zenrad_A,2)])) .* twst.tint_A_s + darks_A;
 twst.raw_B = twst.zenrad_B ./ (twst.cal_B*ones([1,size(twst.zenrad_B,2)])) .* twst.tint_B_s + darks_B;
 
-
+netcdf.close(ncid)
 % figure; plot(twst.wl_A, twst.zenrad_A(:,floor(length(twst.time)./2)),'-', twst.wl_B, twst.zenrad_B(:,floor(length(twst.time)./2)),'-'); logy;
 
 end
