38aaf8d698578b4f31c57fed977063eb16694382
ConnorJFlynn <conjo.flynn@gmail.com>
2024-06-04 20:23:40 -0500

    Update plots_ppt.m

--- plotting/plots_ppt.m
+++ plotting/plots_ppt.m
@@ -11,10 +11,10 @@ set(groot,'DefaultFigureColorMap',jet);
 
 set(0,'DefaultAxesFontName','Tahoma');
 set(0,'DefaultAxesFontSize',16);
-set(0,'DefaultAxesFontWeight','bold');
+% set(0,'DefaultAxesFontWeight','bold');
 set(0,'DefaultTextFontName','Tahoma');
 set(0,'DefaultTextFontSize',16);
-set(0,'DefaultTextFontWeight','bold');
+% set(0,'DefaultTextFontWeight','bold');
 set(0,'DefaultAxesLineWidth',3)
 set(0,'DefaultLineLineWidth',3);
 set(0,'DefaultLineMarkerSize',10);
