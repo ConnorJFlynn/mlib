acdeb03b92037c675f7efba3b1510958b954f32e
ConnorJFlynn <conjo.flynn@gmail.com>
2024-06-04 20:19:17 -0500

    Create ARM_aods_mfr7_v_cim.m

---
+++ mfrsr_proc/ARM_aods_mfr7_v_cim.m
@@ -0,0 +1,333 @@
+
+
+cim = rd_anetaod_v3;
+if ~isfield(cim,'AOD_1640_nm_AOD')&&~isfield(cim,'AOD_1640nm')
+   cim.AOD_1640nm = cim.AOD_1640nm_AOD;
+end
+mfr_files = getfullname('*mfr*aod*','xmfrx_aod');
+
+mfr = anc_load(mfr_files{1});
+qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter1, mfr.vatts.qc_aerosol_optical_depth_filter1);
+   ok = qcs==0 | qcs==1;
+qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter2, mfr.vatts.qc_aerosol_optical_depth_filter2);
+   ok = qcs==0 | qcs==1;
+qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter4, mfr.vatts.qc_aerosol_optical_depth_filter4);
+   ok = qcs==0 | qcs==1;
+qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter5, mfr.vatts.qc_aerosol_optical_depth_filter5);
+   ok = qcs==0 | qcs==1;
+qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter7, mfr.vatts.qc_aerosol_optical_depth_filter7);
+   % ok = qcs==0 | qcs==1;
+mfr_ = anc_sift(mfr, ok);
+
+for m = 2:length(mfr_files)
+   [~,fname,ext] = fileparts(mfr_files{m});
+  
+   mfr = anc_load(mfr_files{m});
+   qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter1, mfr.vatts.qc_aerosol_optical_depth_filter1);
+   ok = qcs==0 ;   
+   qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter2, mfr.vatts.qc_aerosol_optical_depth_filter2);
+   ok = qcs==0 | qcs==1;
+   qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter4, mfr.vatts.qc_aerosol_optical_depth_filter4);
+   ok = qcs==0 | qcs==1;
+   qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter5, mfr.vatts.qc_aerosol_optical_depth_filter5);
+   ok = qcs==0 | qcs==1;
+   qcs = anc_qc_impacts(mfr.vdata.qc_aerosol_optical_depth_filter7, mfr.vatts.qc_aerosol_optical_depth_filter7);
+   % ok = qcs==0 | qcs==1;
+   mfr = anc_sift(mfr, ok);
+    disp(['Read file ',num2str(m),': ',fname, ext, ', Length: ',num2str(length(mfr.time))])
+   mfr_ = anc_cat(mfr_, mfr);
+end
+mfr = mfr_; clear mfr_; 
+bad = mfr.vdata.aerosol_optical_depth_filter1<-1;
+mfr.vdata.aerosol_optical_depth_filter1(bad) = NaN;
+bad = mfr.vdata.aerosol_optical_depth_filter2<-1;
+mfr.vdata.aerosol_optical_depth_filter2(bad) = NaN;
+bad = mfr.vdata.aerosol_optical_depth_filter3<-1;
+mfr.vdata.aerosol_optical_depth_filter3(bad) = NaN;
+bad = mfr.vdata.aerosol_optical_depth_filter4<-1;
+mfr.vdata.aerosol_optical_depth_filter4(bad) = NaN;
+bad = mfr.vdata.aerosol_optical_depth_filter5<-1;
+mfr.vdata.aerosol_optical_depth_filter5(bad) = NaN;
+bad = mfr.vdata.aerosol_optical_depth_filter7<-1;
+mfr.vdata.aerosol_optical_depth_filter7(bad) = NaN;
+[pname, fname, ext] = fileparts(mfr.fname);
+pname = [pname, filesep]; pname = strrep(pname, [filesep filesep], filesep);
+fname = strtok(fname, '.'); 
+fname = [fname, '.',datestr(min(mfr.time),'yyyymmdd_'), datestr(max(mfr.time),'yyyymmdd'),'.mat'];
+save([pname, fname],'-struct','mfr')
+mfr = load([pname, fname]);
+aod = [mfr.vdata.aerosol_optical_depth_filter1;mfr.vdata.aerosol_optical_depth_filter2;mfr.vdata.aerosol_optical_depth_filter3;...
+   mfr.vdata.aerosol_optical_depth_filter4; mfr.vdata.aerosol_optical_depth_filter5; mfr.vdata.aerosol_optical_depth_filter7];
+ang = mfr.vdata.angstrom_exponent;
+figure; ax(1) = subplot(2,1,1); plot(mfr.time, aod,'.');dynamicDateTicks; legend('415','500','615','675','870','1625');
+ax(2) = subplot(2,1,2); plot(mfr.time, ang,'k*'); dynamicDateTicks; legend('angstrom 500 & 870'); 
+linkaxes(ax,'x');
+
+aod = [mfr.vdata.aerosol_optical_depth_filter1;mfr.vdata.aerosol_optical_depth_filter2;mfr.vdata.aerosol_optical_depth_filter3;...
+   mfr.vdata.aerosol_optical_depth_filter4; mfr.vdata.aerosol_optical_depth_filter5; mfr.vdata.aerosol_optical_depth_filter7];
+caod = [cim.AOD_340nm,cim.AOD_380nm, cim.AOD_440nm, cim.AOD_500nm, cim.AOD_675nm, cim.AOD_870nm, cim.AOD_1020nm, cim.AOD_1640nm];
+aod(aod<-10) = NaN; caod(caod<-10) = NaN;
+figure; axx(1) = subplot(2,1,1); plot(mfr.time, aod,'.');dynamicDateTicks; legend('415','500','615','675','870','1625');
+axx(2) = subplot(2,1,2); plot(cim.time, caod,'.'); dynamicDateTicks; legend('340','380','440', '500','675','870','1020','1.6um'); 
+linkaxes(axx,'xy');
+
+aod_ = [mfr.vdata.aerosol_optical_depth_filter1;mfr.vdata.aerosol_optical_depth_filter2;...
+   mfr.vdata.aerosol_optical_depth_filter4; mfr.vdata.aerosol_optical_depth_filter5; mfr.vdata.aerosol_optical_depth_filter7];
+xl = xlim; xl_ = mfr.time>xl(1)&mfr.time<xl(2); xlc = cim.time>xl(1)&cim.time<xl(2);
+mwl = [415,500,675,870,1625]; cwl = [340,380,440,500,675,870,1020,1645];
+aod_bar = nanmean(aod_(:,xl_),2); caod_bar = mean(caod(xlc,:),1)';
+figure; plot(mwl, aod_bar,'o',cwl, caod_bar,'x', ...
+   mwl, 10.^polyval(polyfit(log10(mwl(1:end-1)), log10(aod_bar(1:end-1)),1),log10(mwl)),'b--',...
+   cwl, 10.^polyval(polyfit(log10(cwl(1:end-1)), log10(caod_bar(1:end-1)),1),log10(cwl)),'g--'); logy; logx
+xlabel('wavelength'); ylabel('AOD'); legend('MFRSR','Cimel')
+title({'EPC MFRSR7AOD and Cimel lv 1.5 AOD';datestr(mean(mfr.time(xl_)),'yyyy-mm-dd HH:MM UT')})
+
+figure; hist(ang(ang>0&ang<4), 50)
+figure; plot(mfr.time, [ang; ang_fit],'.'); dynamicDateTicks; 
+
+figure; plot(cim.time(cim.AOD_1640nm>-1), cim.AOD_1640nm(cim.AOD_1640nm>-1),'k.'); dynamicDateTicks; legend('Aeronet 1.6u')
+
+test_mfr = anc_load;
+test_mfr.IQ_time = epoch2serial(test_mfr.vdata.Io_interquartile_time);
+test_mfr.IG_time = epoch2serial(test_mfr.vdata.Io_gauss_time);
+
+figure; plot(test_mfr.IQ_time, test_mfr.vdata.Io_interquartile_values(6,:),'o', ...
+   test_mfr.IG_time, test_mfr.vdata.Io_gauss_values(6,:),'-x' ); dynamicDateTicks
+
+
+aopM2 = anc_bundle_files;
+
+aopS2 = anc_bundle_files;
+
+figure; plot(aopS2.time, aopS2.vdata.Bs_B_1um,'*'); dynamicDateTicks; legend('Bs B')
+
+
+figure; plot(mfr.time, mfr.vdata.aerosol_optical_depth_filter2,'g.');dynamicDateTicks; legend('mfrsr');
+
+mfr = load('D:\aodfit_be\hou\houmfrsr7nchaod1michM1.c1.filtered.mat');
+mfr = load('D:\aodfit_be\epc\epcmfrsr7nchaod1michM1.c1.filtered.mat');
+mfr = load(['D:\aodfit_be\epc\epcmfrsr7nchaod1michM1.c1\epcmfrsr7nchaod1michM1.20230106_20240214.mat.20230106_20240214_noqc.mat']);
+mfr = load(['D:\aodfit_be\epc\epcmfrsr7nchaod1michM1.c1\epcmfrsr7nchaod1michM1.20230106_20240214.mat']);
+ang = mfr.vdata.angstrom_exponent;
+
+figure; hist(ang(ang>0&ang<2.5),50)
+
+% Evaluate from here...
+ang_lte = find(ang<.5);
+ang_gte = find(ang>.5);
+[cinm, minc] = nearest(cim.time, mfr.time);
+[cinm, minc] = nearest(cim.time, mfr.time(ang_lte));
+
+X = cim.AOD_1640nm(cinm);
+Y = mfr.vdata.aerosol_optical_depth_filter7(ang_lte(minc));
+x_str = 'Cimel AOD'; y_str = 'MFRSR AOD'; t_str = 'Aerosol Optical Depth 1.6 um';
+
+
+if size(X,1)==size(Y,2); Y = Y'; end
+bad = X<=-1 | Y<=-1; X(bad) =[]; Y(bad) = [];
+D = den2plot(X,Y);
+figure; scatter(X, Y,6,log10(D)); colormap(comp_map_w_jet);
+xlabel(x_str); ylabel(y_str); title(t_str)
+ xl = xlim; yl = ylim; xlim([0,round(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+[good, P_bar] = rbifit(X,Y,3,0); hold('on');  plot(xl,polyval(P_bar,xl),'b-', 'linewidth',3); hold('off')
+
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+gt.Units = 'Normalized'; gt.Position = round(gt.Position.*10)./10;
+
+% ... to here
+
+
+
+
+
+X = cim.AOD_500nm(cinm);
+Y = mfr.vdata.aerosol_optical_depth_filter2(minc);
+x_str = 'Cimel AOD'; y_str = 'MFRSR AOD'; t_str = 'Aerosol Optical Depth 500 nm';
+
+X = cim.AOD_675nm(cinm);
+Y = mfr.vdata.aerosol_optical_depth_filter4(minc);
+x_str = 'Cimel AOD'; y_str = 'MFRSR AOD'; t_str = 'Aerosol Optical Depth 675 nm';
+
+X = cim.AOD_870nm(cinm);
+Y = mfr.vdata.aerosol_optical_depth_filter5(minc);
+x_str = 'Cimel AOD'; y_str = 'MFRSR AOD'; t_str = 'Aerosol Optical Depth 870 nm';
+
+
+
+
+% X = cim.AOD_870nm_AOD(cins);
+% Y = sas.vdata.aerosol_optical_depth(7,sinc);
+% x_str = 'Cimel AOD'; y_str = 'SASHe AOD'; t_str = 'Aerosol Optical Depth 870 nm';
+
+% X = mfr.vdata.aerosol_optical_depth_filter5(mins);
+% Y = sas.vdata.aerosol_optical_depth(7,sinm);
+% x_str = 'MFRSR AOD'; y_str = 'SASHe AOD'; t_str = 'Aerosol Optical Depth 870 nm';
+
+
+% Evaluate from here...
+if size(X,1)==size(Y,2); Y = Y'; end
+bad = X<=0 | Y<=0; X(bad) =[]; Y(bad) = [];
+D = den2plot(X,Y);
+figure; scatter(X, Y,6,log10(D)); colormap(comp_map_w_jet);
+xlabel(x_str); ylabel(y_str); title(t_str)
+ xl = xlim; yl = ylim; xlim([0,round(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+[good, P_bar] = rbifit(X,Y,3,0); hold('on');  plot(xl,polyval(P_bar,xl),'b-', 'linewidth',3); hold('off')
+
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+gt.Units = 'Normalized'; gt.Position = round(gt.Position.*10)./10;
+
+% ... to here
+
+sas = load(['D:\aodfit_be\pvc\pvcsashevisaodM1.c1.filt.mat']);
+
+
+early_M = find(serial2doys(mfr.time)<500); early_S = find(serial2doys(sas.time)<500);
+[mins, sinm] = nearest(mfr.time(early_M), sas.time(early_S));
+early_M = early_M(mins), early_S = early_S(sinm);
+
+
+% 500 nm
+figure; scatter(cim.AOD_500nm_AOD(cinm), mfr.vdata.aerosol_optical_depth_filter2(minc),6,serial2doys(cim.time(cinm)));
+xlabel('cim'); ylabel('mfr'); title('500 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(cim.AOD_500nm_AOD(cins), sas.vdata.aerosol_optical_depth(4,sinc),6,serial2doys(cim.time(cins)));
+xlabel('cim'); ylabel('sas');  title('500 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(mfr.vdata.aerosol_optical_depth_filter2(mins), sas.vdata.aerosol_optical_depth(4,sinm),6,serial2doys(mfr.time(mins)));
+xlabel('mfr'); ylabel('sas');  title('500 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+% 675 nm
+figure; scatter(cim.AOD_675nm_AOD(cinm), mfr.vdata.aerosol_optical_depth_filter4(minc),6,serial2doys(cim.time(cinm)));
+xlabel('cim'); ylabel('mfr'); title('675 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(cim.AOD_675nm_AOD(cins), sas.vdata.aerosol_optical_depth(6,sinc),6,serial2doys(cim.time(cins)));
+xlabel('cim'); ylabel('sas');  title('675 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(mfr.vdata.aerosol_optical_depth_filter4(mins), sas.vdata.aerosol_optical_depth(6,sinm),6,serial2doys(mfr.time(mins)));
+xlabel('mfr'); ylabel('sas');  title('675 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+
+% 870 nm
+figure; scatter(cim.AOD_870nm_AOD(cinm), mfr.vdata.aerosol_optical_depth_filter5(minc),6,serial2doys(cim.time(cinm)));
+xlabel('cim'); ylabel('mfr'); title('870 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(cim.AOD_870nm_AOD(cins), sas.vdata.aerosol_optical_depth(5,sinc),6,serial2doys(cim.time(cins)));
+xlabel('cim'); ylabel('sas');  title('870 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+figure; scatter(mfr.vdata.aerosol_optical_depth_filter4(mins), sas.vdata.aerosol_optical_depth(4,sinm),6,serial2doys(mfr.time(mins)));
+xlabel('mfr'); ylabel('sas');  title('870 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+
+figure; scatter(mfr.vdata.aerosol_optical_depth_filter2(mins(ij)), sas.vdata.aerosol_optical_depth(4,sinm(ij)),6,serial2doys(mfr.time(mins(ij))));
+xlabel('mfr'); ylabel('sas');  title('500 nm')
+ xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+early_M = find(serial2doys(mfr.time)<500); early_S = find(serial2doys(sas.time)<500);
+[mins, sinm] = nearest(mfr.time(early_M), sas.time(early_S));
+early_M = early_M(mins), early_S = early_S(sinm);
+ 
+
+
+
+
+sas.vdata.wavelength
+D_415 = den2plot(mfr.vdata.aerosol_optical_depth_filter1(early_M), sas.vdata.aerosol_optical_depth(2,early_S));
+figure; scatter(mfr.vdata.aerosol_optical_depth_filter1(early_M), sas.vdata.aerosol_optical_depth(2,early_S),4,log10(D_415)); colormap(comp_map_w_jet);
+[good_415,P_415] = rbifit(mfr.vdata.aerosol_optical_depth_filter1(early_M), sas.vdata.aerosol_optical_depth(2,early_S),3);
+[gt,txt, stats] = txt_stat(mfr.vdata.aerosol_optical_depth_filter1(early_M(good_415)), sas.vdata.aerosol_optical_depth(2,early_S(good_415)),P_415); 
+set(gt,'color','b');
+hold('on'); 
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square');
+hold('on'); xl = xlim; li = plot(xl, xl, 'k:','linewidth',4); hold('off')
+
+% Now try SAS vs MFRSR
+xiny = early_M; yinx = early_S;
+title_str_top = 'HOU M1 MFRSR7nch vs SASHe'; title_str_bot = 'AOD at 500 nm'; 
+x_str = 'MFRSR AOD'; y_str = 'SASHe AOD';
+X = mfr.vdata.aerosol_optical_depth_filter2(xiny); 
+Y = sas.vdata.aerosol_optical_depth(4,yinx);
+D = den2plot(X, Y);
+[good,P_bar] = rbifit(X, Y,3,0);
+figure; scatter(X, Y,4,log10(D),'filled'); colormap(comp_map_w_jet);
+% figure; scatter(X, Y,4,floor(mfr.time(xiny)),'filled'); colormap(comp_map_w_jet);
+cv = caxis; caxis([0.31,cv(2)]);
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+hold('on'); plot(ylim, ylim, 'k--',ylim, polyval(P_bar,ylim),'b-'); hold('off')
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+title({title_str_top; title_str_bot});
+xlabel(x_str); ylabel(y_str);
+% adjust zoom if desired
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+
+xiny = early_M; yinx = early_S;
+title_str_top = 'HOU M1 MFRSR7nch vs SASHe'; title_str_bot = 'AOD at 615 nm'; 
+x_str = 'MFRSR AOD'; y_str = 'SASHe AOD';
+X = mfr.vdata.aerosol_optical_depth_filter3(xiny); 
+Y = sas.vdata.aerosol_optical_depth(5,yinx);
+D = den2plot(X, Y);
+[good,P_bar] = rbifit(X, Y,3,0);
+figure; scatter(X, Y,4,log10(D),'filled'); colormap(comp_map_w_jet);
+% figure; scatter(X, Y,4,floor(mfr.time(xiny)),'filled'); colormap(comp_map_w_jet);
+cv = caxis; caxis([0.31,cv(2)]);
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+hold('on'); plot(ylim, ylim, 'k--',ylim, polyval(P_bar,ylim),'b-'); hold('off')
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+title({title_str_top; title_str_bot});
+xlabel(x_str); ylabel(y_str);
+% adjust zoom if desired
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+
+xiny = early_M; yinx = early_S;
+title_str_top = 'HOU M1 MFRSR7nch vs SASHe'; title_str_bot = 'AOD at 675 nm'; 
+x_str = 'MFRSR AOD'; y_str = 'SASHe AOD';
+X = mfr.vdata.aerosol_optical_depth_filter4(xiny); 
+Y = sas.vdata.aerosol_optical_depth(6,yinx);
+D = den2plot(X, Y);
+[good,P_bar] = rbifit(X, Y,3,0);
+figure; scatter(X, Y,4,log10(D),'filled'); colormap(comp_map_w_jet);
+% figure; scatter(X, Y,4,floor(mfr.time(xiny)),'filled'); colormap(comp_map_w_jet);
+cv = caxis; caxis([0.31,cv(2)]);
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+hold('on'); plot(ylim, ylim, 'k--',ylim, polyval(P_bar,ylim),'b-'); hold('off')
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+title({title_str_top; title_str_bot});
+xlabel(x_str); ylabel(y_str);
+% adjust zoom if desired
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+
+xiny = early_M; yinx = early_S;
+title_str_top = 'HOU M1 MFRSR7nch vs SASHe'; title_str_bot = 'AOD at 870 nm'; 
+x_str = 'MFRSR AOD'; y_str = 'SASHe AOD';
+X = mfr.vdata.aerosol_optical_depth_filter5(xiny); 
+Y = sas.vdata.aerosol_optical_depth(7,yinx);
+D = den2plot(X, Y);
+[good,P_bar] = rbifit(X, Y,3,0);
+figure; scatter(X, Y,4,log10(D),'filled'); colormap(comp_map_w_jet);
+% figure; scatter(X, Y,4,floor(mfr.time(xiny)),'filled'); colormap(comp_map_w_jet);
+cv = caxis; caxis([0.31,cv(2)]);
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+hold('on'); plot(ylim, ylim, 'k--',ylim, polyval(P_bar,ylim),'b-'); hold('off')
+[gt,txt, stats] = txt_stat(X(good), Y(good),P_bar); set(gt,'color','b');
+title({title_str_top; title_str_bot});
+xlabel(x_str); ylabel(y_str);
+% adjust zoom if desired
+xl = xlim; yl = ylim; xlim([0,floor(10.*min([xl(2),yl(2)]))./10]);ylim(xlim); axis('square')
+
+
